a6fa5bb0522de6d28efa2e85354b480fa0b89f11
mohayonao <mohayonao@gmail.com>
2015-07-17 20:09:37 +0900

    fix exports

--- test/index.js
+++ test/index.js
@@ -1,15 +1,19 @@
 import assert from "power-assert";
-import index from "../src";
-import Dispatcher from "../src/Dispatcher";
-import Delegator from "../src/Delegator";
-import Duplex from "../src/Duplex";
+import index, { Dispatcher, Delegator, Duplex } from "../src";
+import Dispatcher2 from "../src/Dispatcher";
+import Delegator2 from "../src/Delegator";
+import Duplex2 from "../src/Duplex";
 
 describe("index", () => {
   describe("classes", () => {
     it("works", () => {
-      assert(index === Dispatcher);
-      assert(index.Delegator === Delegator);
-      assert(index.Duplex === Duplex);
+      assert(index === Dispatcher2);
+      assert(index.Dispatcher === Dispatcher2);
+      assert(index.Delegator === Delegator2);
+      assert(index.Duplex === Duplex2);
+      assert(Dispatcher === Dispatcher2);
+      assert(Delegator === Delegator2);
+      assert(Duplex === Duplex2);
     });
   });
 });
